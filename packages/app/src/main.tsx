67a12b0e29f7c0692f404eae74d6a8da71f5aba1
skylineagle <barmghr@gmail.com>
2025-03-02 23:11:23 +0200

    tweak: react-scan only in dev

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { scan } from "react-scan"; // must be imported before React and React DOM
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+scan({
+  enabled: process.env.NODE_ENV !== "production",
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
