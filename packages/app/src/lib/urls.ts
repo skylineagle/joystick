1d9e430ebd3f43180d27b808bd25c7cba51beff8
skylineagle <barmghr@gmail.com>
2025-03-03 00:04:50 +0200

    feat: reverse proxy

--- packages/app/src/lib/urls.ts
+++ packages/app/src/lib/urls.ts
@@ -12,7 +12,11 @@ function getServiceUrl(service: keyof typeof PORTS): string {
   const hostname = window.location.hostname;
   const port = PORTS[service];
 
-  return `${protocol}//${hostname}:${port}`;
+  if (import.meta.env.NODE_ENV === "development") {
+    return `${protocol}//${hostname}:${port}`;
+  }
+
+  return `${protocol}//${hostname}/${port}`;
 }
 
 export const urls = {
