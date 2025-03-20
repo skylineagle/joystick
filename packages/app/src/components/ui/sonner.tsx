fab4d31e1bbdd105bdd90219bcb889b68ecdc8ae
skylineagle <barmghr@gmail.com>
2025-03-21 00:02:40 +0200

    fix: build error

--- packages/app/src/components/ui/sonner.tsx
+++ packages/app/src/components/ui/sonner.tsx
@@ -6,7 +6,8 @@ import { Toaster as Sonner } from "sonner";
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
+  const { getActualColorMode } = useTheme();
+  const theme = getActualColorMode();
 
   return (
     <Sonner
