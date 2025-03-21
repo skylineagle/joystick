9ff45cdf5fdbf6d3a14686b97b3d60e934d3d3cd
skylineagle <barmghr@gmail.com>
2025-03-22 00:10:46 +0200

    fix: build hangs

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
-import checker from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), checker({ typescript: true })],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
