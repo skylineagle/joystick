36ab5d28b523122b2244710970a4ae4a9d44f0c5
horizon <55806431+skylineagle@users.noreply.github.com>
2025-03-31 12:02:09 +0300

    fix: disable urls by path

--- packages/app/src/lib/urls.ts
+++ packages/app/src/lib/urls.ts
@@ -23,7 +23,7 @@ function getServiceUrl(service: keyof typeof PORTS): string {
   const hostname = window.location.hostname;
 
   return `http://${hostname}${
-    Object.keys(PREFIXES).includes(service) && import.meta.env.PROD
+    false && Object.keys(PREFIXES).includes(service) && import.meta.env.PROD
       ? `/${PREFIXES[service as keyof typeof PREFIXES]}`
       : `:${PORTS[service]}`
   }`;
