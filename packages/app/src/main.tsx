09a689e542f53761d1ee51a17953057ddb546b43
skylineagle <barmghr@gmail.com>
2025-03-31 23:30:13 +0300

    fix: allow react-roi to work

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,13 +1,21 @@
-import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { scan } from "react-scan";
+import { v4 as uuidv4 } from "uuid";
 import App from "./App.tsx";
+
+import "./index.css";
 
 scan({
   enabled: import.meta.env.NODE_ENV !== "production",
   trackUnnecessaryRenders: true,
 });
+
+if (typeof crypto !== "undefined" && typeof crypto.randomUUID === "undefined") {
+  crypto.randomUUID = function () {
+    return uuidv4() as `${string}-${string}-${string}-${string}-${string}`;
+  };
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
