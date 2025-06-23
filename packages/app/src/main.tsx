816e5ed865b352b450de8c5367bf3fa961dffb8a
skylineagle <skylineagle@proton.me>
2025-06-23 11:46:30 +0300

    feat: added meticulous

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,15 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { scan } from "react-scan";
+import { tryLoadAndStartRecorder } from "@alwaysmeticulous/recorder-loader";
 import { v4 as uuidv4 } from "uuid";
 import App from "./App.tsx";
 
 import "./index.css";
 
-scan({
-  enabled: import.meta.env.NODE_ENV !== "production",
-  trackUnnecessaryRenders: true,
-});
+if (import.meta.env.NODE_ENV !== "production") {
+  await tryLoadAndStartRecorder({
+    recordingToken: "JCdtD4mxCPMI3Hq0nZhauj6yVRp1Y3s7uQ67Ku95",
+    isProduction: false,
+    forceRecording: true,
+  });
+
+  scan({
+    enabled: import.meta.env.NODE_ENV !== "production",
+    trackUnnecessaryRenders: true,
+  });
+}
 
 if (typeof crypto !== "undefined" && typeof crypto.randomUUID === "undefined") {
   crypto.randomUUID = function () {
