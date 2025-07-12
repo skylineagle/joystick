3ece59f0f2e10ea63054f8df64111627f0783ad0
skylineagle <skylineagle@proton.me>
2025-07-12 13:00:36 +0300

    fix: another try

--- packages/app/src/vite-env.d.ts
+++ packages/app/src/vite-env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_INNGEST_URL?: string;
-  readonly VITE_INNGEST_EVENT_KEY?: string;
-  readonly VITE_POCKETBASE_URL: string;
-  readonly VITE_STREAM_URL: string;
-  readonly VITE_JOYSTICK_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
