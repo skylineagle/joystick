153c6493cd7e6f7b2eaccb87b65fc9ddcbe917ea
horizon <55806431+skylineagle@users.noreply.github.com>
2025-07-21 11:25:49 +0300

    Update configuration-editor.tsx

--- packages/app/src/components/configuration/configuration-editor.tsx
+++ packages/app/src/components/configuration/configuration-editor.tsx
@@ -200,7 +200,7 @@ export function ConfigurationEditor({ device }: ConfigurationEditorProps) {
               user: "",
               password: "",
               host: "",
-              phone: "",
+              phone: 22,
             },
           });
         }
